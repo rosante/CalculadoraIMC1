3fee6770e393d83d8f438e9850ed32a49e2ba11b
rosante <rafarosante@hotmail.com>
2021-07-25 21:57:36 -0300

    Alterado algumas funcionalidades e retorno

--- app/src/main/java/com/ruzzante/calculadoradepeso/MainActivity.kt
+++ app/src/main/java/com/ruzzante/calculadoradepeso/MainActivity.kt
@@ -13,30 +13,29 @@ class MainActivity : AppCompatActivity() {
 
 
         btnCalculate.setOnClickListener{
-            val gender = if (radioFemale.isChecked) "female"; else { "male" }
             val message = textViewResult
             val weight = textWeight.text.toString()
             val height = textHeight.text.toString()
-            message.setText(Calculate(gender, weight=weight, height=height))
-            //message.setText("$gender $weight $height")
+            message.setText(Calculate(weight=weight, height=height))
         }
 
 
     }
 
-    fun Calculate(gender:String, height:String, weight:String):String{
-        if (gender == "male"){
-            val imc = weight.toDouble()/(height.toDouble()*height.toDouble())
-            val message:String
-            when {
-                imc < (18.5).toDouble() -> message="Magreza"
-                imc in (18.5).toDouble()..(24.9).toDouble() -> message="Peso Normal"
-                imc in (25).toDouble()..(30).toDouble() -> message="Acima do Peso"
-                imc > 30.toDouble() -> message="Obeso"
-                else -> message="Erro"
-            }
-            return message
+    fun Calculate(height:String, weight:String):String{
+        val imc = weight.toDouble()/(height.toDouble()*height.toDouble())
+        val pesoIdealMin = (height.toDouble()*height.toDouble())*(18.5)
+        val pesoIdealMax = (height.toDouble()*height.toDouble())*(25)
+        var message:String
+        when {
+            imc < (18.5).toDouble() -> message="Magreza"
+            imc in (18.5).toDouble()..(25).toDouble() -> message="Peso Normal"
+            imc in (25).toDouble()..(30).toDouble() -> message="Acima do Peso"
+            imc in (30).toDouble()..(40).toDouble() -> message="Obeso"
+            imc > (40).toDouble() -> message="Muito Obeso"
+            else -> message="Erro"
         }
-        return "Erro"
+        message +=  "\nSeu peso ideal seria entre ${pesoIdealMin.toInt().toString()} Kgs e ${pesoIdealMax.toInt().toString()} Kgs"
+        return message
     }
 }
